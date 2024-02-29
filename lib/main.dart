60828ecfbb3f7d13d7028e632048a83da439103e
Aiden <97030039+AidenDexter@users.noreply.github.com>
2024-02-29 17:36:50 +0400

    feat: add welcome page

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_bloc_groupchat/app_blocs.dart';
 import 'package:flutter_bloc_groupchat/app_events.dart';
+import 'package:flutter_bloc_groupchat/pages/welcome/bloc/welcome_blocs.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'app_states.dart';
+import 'pages/welcome/welcome.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,15 +18,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => AppBlocs(),
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
+      create: (context) => WelcomeBloc(),
+      child: ScreenUtilInit(
+        builder: (context, child) => const MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: Welcome(),
         ),
-        home: const MyHomePage(),
       ),
     );
   }
